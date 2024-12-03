38ec790599dbbf3b43981b1d4ee82800e0e75d96
greskr <161820706+greskr@users.noreply.github.com>
2024-12-03 14:22:25 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ToXJ9bwjFhLsH4TcV_p-e0hgU-QOZLpnMFNd8PUrGlk/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTFUqeLBBKR7SeAov6CsqL6qRfNd8Dk2tZXE-A9AdsMQ-pDewD9nVirnGvepiwOJ2m4-LT_qKizn0a4/pubhtml';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
